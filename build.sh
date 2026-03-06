04f54450b9750f2cc2c6e158dc4dbe68d3054004
charlieholy <chenpeiwen6@gmail.com>
2026-03-06 08:52:39 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,15 @@
+ubuntu22
+
+sudo apt update
+sudo apt install -y \
+git python3 curl pkg-config ninja-build build-essential \
+libasound2-dev libpulse-dev libx11-dev libxext-dev libxi-dev \
+libxrandr-dev libxcursor-dev libxdamage-dev libxcomposite-dev \
+libxfixes-dev libxrender-dev libxinerama-dev libatk1.0-dev \
+libatk-bridge2.0-dev libcups2-dev libdbus-1-dev libexpat1-dev \
+libfontconfig1-dev libfreetype6-dev libpng-dev libssl-dev \
+libudev-dev libvulkan-dev libglib2.0-dev
+
 git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
 export PATH=$PWD/depot_tools:$PATH
 
